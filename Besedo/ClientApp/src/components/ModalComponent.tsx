1aa0da3ca52926f1a0cb80f0d22c143f806aa1d4
timmu <tim.muscat.95@gmail.com>
2022-09-08 22:19:27 +0200

    Will now navigate back to root if id in edit path parameter does not correspond to any table record.

--- Besedo/ClientApp/src/components/ModalComponent.tsx
+++ Besedo/ClientApp/src/components/ModalComponent.tsx
@@ -45,18 +45,27 @@ const ModalComponent = ({ mode }: Props) => {
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   const ipRegex =
     /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
-  
+
   const _id = useMatch("/edit/:id")?.params.id;
   const id = parseInt(_id ?? "NaN");
 
-  
-    if (!isNaN(id)) {
-        const u = users.find((u) => u.id === id);
-        form.setFieldsValue(u);
+  if (!isNaN(id)) {
+    const u = users.find((u) => u.id === id);
+
+    if (!u && mode === ModalTypes.Modify && users.length > 0) {
+      navigate("/");
     }
+    form.setFieldsValue(u);
+  }
 
   return (
-    <Modal title={title} open={true} onCancel={onModalCancel} footer={null} destroyOnClose={true}>
+    <Modal
+      title={title}
+      open={true}
+      onCancel={onModalCancel}
+      footer={null}
+      destroyOnClose={true}
+    >
       <Form form={form} onFinish={onFormFinish.bind(this, id)}>
         <Form.Item label="Name" name="name" rules={validationRules}>
           <Input placeholder="John" />
